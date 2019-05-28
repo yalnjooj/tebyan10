e1a848bc830a418d953b00124c2504315389d730
omar husawi <yalnjooj@gmail.com>
2019-05-28 03:03:03 +0300

    after adding the scroller and fonts

--- src/app/components/mainSystem/home/home.component.ts
+++ src/app/components/mainSystem/home/home.component.ts
@@ -17,6 +17,21 @@ export class HomeComponent implements OnInit {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+// اعادة تعيين حجم الصحفة
   height(){
     // set height of body
 // offsetHeight -- clientHeight
@@ -24,13 +39,12 @@ export class HomeComponent implements OnInit {
 // document.documentElement.clientHeight;
 let navbar = document.getElementById('navbar-h').clientHeight;
 let logosRow = document.getElementById('bodyRow-h').clientHeight;
-let cenerSpases = document.getElementById('footer-h').clientHeight;
-let cenerSpase = cenerSpases + 0;
+let cenerSpase = document.getElementById('footer-h').clientHeight;
 let win = window.document.body.clientHeight;
 let newHeight = win - (navbar + logosRow + cenerSpase);
 
 document.getElementById("containerHeight").style.height = newHeight+"px";
-document.getElementById("bodyRow-h").style.height = 519+"px";
+document.getElementById("bodyRow-h").style.height = 524 +"px";
 
 }
 
